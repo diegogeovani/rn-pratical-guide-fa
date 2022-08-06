fa25e86cc420cc90cef7d6916fe930f69415a827
Diego Santos <diego.santos@olxbr.com>
2022-08-06 14:53:07 -0300

    list of goals

--- first-app/App.js
+++ first-app/App.js
@@ -3,17 +3,27 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
+  const [enteredGoal, setEnteredGoal] = useState('')
+
+  const goalInputHandler = (text) => {
+    setEnteredGoal(text)
+  }
+  const addGoalHandler = () => {
+    console.log(enteredGoal)
+  }
+
   return (
     <View style={styles.container}>
-      <View>
-        <Text>{outputText}</Text>
-        <Button
-          title='Change Text'
-          onPress={() => setOutputText('My new text')} />
-          <View style={styles.inputContainer}>
-            <TextInput placeholder="Your course goal" style={styles.textInput} />
-            <Button title="Add goal" />
-          </View>
+      <Text>{outputText}</Text>
+      <Button
+        title='Change Text'
+        onPress={() => setOutputText('My new text')} />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Your course goal" style={styles.textInput} onChangeText={goalInputHandler} />
+        <Button title="Add goal" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of goals...</Text>
       </View>
     </View>
   );
@@ -23,17 +33,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    padding: 32,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
     marginTop: 16,
+    marginBottom: 24,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#ccc',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
